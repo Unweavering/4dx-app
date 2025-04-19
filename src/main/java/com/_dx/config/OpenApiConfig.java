af0a6f77ca89d14286b7d487490d4c92a487314f
kjh <kbskjh2@gmail.com>
2025-04-20 04:47:51 +0900

    docs: add comments

--- src/main/java/com/_dx/config/OpenApiConfig.java
+++ src/main/java/com/_dx/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+//Swagger는 기본적으로 보안 스키마(Security Scheme) 설정이 있어야 상단에 Authorize 버튼을 생성
 @OpenAPIDefinition(
         info = @Info(title = "4DX API", version = "v1", description = "4DX 목표 관리 시스템 API 명세")
 )
